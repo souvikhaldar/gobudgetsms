6a79e8e1793fa7fc721a70b16fbc0c64058d759f
Souvik Haldar <souvik.haldar@kartbites.com>
2018-07-17 20:20:25 +0530

    exported the fields of details

--- factory.go
+++ factory.go
@@ -7,16 +7,16 @@ import (
 
 // Details has all the credentials of the Budget SMS account as well as the details regarding the SMS
 type Details struct {
-	username string
-	userid   string
-	handle   string
-	msg      string
-	from     string
-	to       string
-	customid string
-	price    int
-	mccmnc   int
-	credit   int
+	Username string
+	Userid   string
+	Handle   string
+	Msg      string
+	From     string
+	To       string
+	Customid string
+	Price    int
+	Mccmnc   int
+	Credit   int
 }
 
 func createmsg(msg string) string {
@@ -33,7 +33,7 @@ func buildURL(det Details) string {
 		Scheme:   "https",
 		Host:     "api.budgetsms.net",
 		Path:     "/sendsms",
-		RawQuery: fmt.Sprintf("username=%s&userid=%s&handle=%s&msg=%s&from=%s&to=%s&customid=%s&price=%d&mccmnc=%d&credit=%d", det.username, det.userid, det.handle, createmsg(det.msg), det.from, det.to, det.customid, det.price, det.mccmnc, det.credit),
+		RawQuery: fmt.Sprintf("username=%s&userid=%s&handle=%s&msg=%s&from=%s&to=%s&customid=%s&price=%d&mccmnc=%d&credit=%d", det.Username, det.Userid, det.Handle, createmsg(det.Msg), det.From, det.To, det.Customid, det.Price, det.Mccmnc, det.Credit),
 	}
 	fmt.Println("Final URL is", u.String())
 	return u.String()
