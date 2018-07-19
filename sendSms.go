01d5a352e8790e369dc0d352dcd68167c2914211
Souvik Haldar <souvik.haldar@kartbites.com>
2018-07-19 18:07:20 +0530

    logged

--- sendSms.go
+++ sendSms.go
@@ -2,12 +2,14 @@ package gobudgetsms
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 // SendSMS returns the response after requesting Budget SMS api and the error if any
 func SendSMS(details Details, message, to, from string) (string, error) {
 	url := buildURL(detail, message, to, from)
+	log.Println("The URL formed is ", url)
 	res, er := http.Get(url)
 	if er != nil {
 		return "", er
